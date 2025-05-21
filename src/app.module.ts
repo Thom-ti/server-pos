bbf0415004d39cbf6ac56491bd836485e54c23aa
Thom-ti <ti.thomsan@gmail.com>
2025-05-21 10:51:32 +0700

    generate resource - products, cart, checkout

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pos-db')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pos-db'),
+    ProductsModule,
+    CartModule,
+    CheckoutModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
