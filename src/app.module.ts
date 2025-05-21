daef1cbfb86fa941f1770c8d53cdd07b3fa15570
Thom-ti <ti.thomsan@gmail.com>
2025-05-21 14:23:44 +0700

    set env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { CheckoutModule } from './checkout/checkout.module';
 import { CartItemsModule } from './cart-items/cart-items.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/pos-db'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     ProductsModule,
     CheckoutModule,
     CartItemsModule,
